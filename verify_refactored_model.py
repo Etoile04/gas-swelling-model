6fb847899d17db2c3383c10d392a1e70feec82d6
Etoile04 <lwj04@tsinghua.org.cn>
2026-03-20 13:55:03 +0800

    Cleanup legacy files and build artifacts

--- verify_refactored_model.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Verification script for refactored model import"""
-
-import sys
-sys.path.append('/Users/lwj04/Library/Python/3.9/lib/python/site-packages')
-sys.path.insert(0, '.')
-
-from gas_swelling.models.refactored_model import RefactoredGasSwellingModel
-print('refactored model OK')
